7c2842579b878a54f65ce0508e5d9170df1e2aa8
Reycel Carcueva <rvcarcueva2@student.apc.edu.ph>
2025-07-06 22:27:18 +0800

    modified footer

--- web/app/components/Footer.tsx
+++ web/app/components/Footer.tsx
@@ -3,55 +3,163 @@
 import { useEffect, useState } from 'react';
 import React from "react";
 import Link from "next/link";
-
-
-
+import Image from 'next/image';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone, faCircleChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 
 const Footer = () => {
 
     return (
-        <footer className=" bg-whitemt-10">
-            <div className="text-black max-w-7xl mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 text-sm py-10" >
+        <footer className="bg-white mt-10">
+            <div className="text-black max-w-7xl mx-auto px-4 grid grid-flow-row md:grid-flow-col auto-cols-auto gap-8 text-sm py-10 overflow-hidden">
 
-             
-                <div>
-                    <h4 className=" mb-3">About Us</h4>
-                    <ul className="space-y-2">
-                        <li><a href="#" className="hover:underline">Our Story</a></li>
-                        <li><a href="#" className="hover:underline">Mission & Vision</a></li>
-                        <li><a href="#" className="hover:underline">Leadership</a></li>
-                    </ul>
+
+                <div className=" font-geist">
+                    <div className="flex items-center">
+                        <Image
+                            src="/PTI-Logo.png"
+                            alt="Pilipinas Taekwondo"
+                            width={136}
+                            height={136}
+                            className="rounded-full"
+                        />
+                    </div>
                 </div>
 
-             
-                <div className="font-geist">
+
+                <div className=" font-geist col-span-2">
+
+
+                    <h4 className="mb-4 text-sm text-gray-800">
+                        Get in touch with us and stay updated.
+                    </h4>
+
+
+                    <form className="flex items-center border border-gray-300 bg-gray-100 rounded w-full">
+                        <input
+                            type="email"
+                            placeholder="Enter your email"
+                            required
+                            className="flex-1 px-4 py-2 text-sm text-gray-800 bg-transparent outline-none"
+                        />
+                        <button
+                            type="submit"
+                            className="px-4 text-[#FED018] hover:text-[#EAB044] cursor-pointer"
+                        >
+                            <FontAwesomeIcon icon={faCircleChevronRight} className="w-5 h-5" />
+                        </button>
+                    </form>
+
+                </div>
+
+
+                <div className="font-geist flex flex-col items-center">
                     <ul className="text-[18px] font-semibold space-y-2">
-                        <li><a href="#" className="hover:underline">Home</a></li>
-                        <li><a href="#" className="hover:underline">About</a></li>
-                        <li><a href="#" className="hover:underline">Competitions</a></li>
-                        <li><a href="#" className="hover:underline">News</a></li>
-                        
+                        <li>
+                            <Link href="/" className="relative group">
+                                <span className="relative transition">
+                                    Home
+                                    <span className="absolute left-1/2 -bottom-1 w-0 h-[2.5px] bg-[#FED018] transition-all duration-300 group-hover:w-full group-hover:-translate-x-1/2"></span>
+                                </span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/about" className="relative group">
+                                <span className="relative transition">
+                                    About
+                                    <span className="absolute left-1/2 -bottom-1 w-0 h-[2.5px] bg-[#FED018] transition-all duration-300 group-hover:w-full group-hover:-translate-x-1/2"></span>
+                                </span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/competitions" className="relative group">
+                                <span className="relative transition">
+                                    Competitions
+                                    <span className="absolute left-1/2 -bottom-1 w-0 h-[2.5px] bg-[#FED018] transition-all duration-300 group-hover:w-full group-hover:-translate-x-1/2"></span>
+                                </span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/news" className="relative group">
+                                <span className="relative transition">
+                                    News
+                                    <span className="absolute left-1/2 -bottom-1 w-0 h-[2.5px] bg-[#FED018] transition-all duration-300 group-hover:w-full group-hover:-translate-x-1/2"></span>
+                                </span>
+                            </Link>
+                        </li>
                     </ul>
                 </div>
 
-            
-                <div className="font-geist">
+
+                <div className="font-geist flex flex-col items-center mr-5">
                     <h4 className="font-semibold mb-3">Follow Us</h4>
-                    <ul className="space-y-2">
-                        <li><a href="#" className="hover:underline">FAQs</a></li>
-                        <li><a href="#" className="hover:underline">Contact Us</a></li>
-                        <li><a href="#" className="hover:underline">Privacy Policy</a></li>
-                    </ul>
+                    {/* Social Media */}
+                    <div className="flex items-center gap-1">
+                        <a
+                            href="https://www.facebook.com/profile.php?id=100071547405103"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Image
+                                src="/icons/Facebook-bg.svg"
+                                alt="Facebook"
+                                width={26}
+                                height={26}
+                                className="hover:opacity-80 transition"
+                            />
+                        </a>
+
+                        <a
+                            href="https://www.instagram.com/pilipinas_taekwondo_inc"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Image
+                                src="/icons/Instagram-bg.svg"
+                                alt="Instagram"
+                                width={26}
+                                height={26}
+                                className="hover:opacity-80 transition"
+                            />
+                        </a>
+
+                        <a
+                            href="https://www.youtube.com/@pilipinastaekwondo"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Image
+                                src="/icons/Youtube-bg.svg"
+                                alt="YouTube"
+                                width={26}
+                                height={26}
+                                className="hover:opacity-80 transition"
+                            />
+                        </a>
+                    </div>
                 </div>
 
-              
-                <div className="font-geist">
-                    <h4 className="font-semibold mb-3">Contact Us</h4>
-                    <p className="mb-2">pilipinastaekwondo@gmail.com</p>
-                    <p>+63 912 345 6789</p>
-                    
+
+                <div className="font-geist ">
+                    <h4 className="font-semibold mb-3">Follow Us</h4>
+
+                    <div className="flex items-center gap-2 mb-2">
+                        <FontAwesomeIcon icon={faEnvelope} className="w-4 h-4" />
+                        <a
+                            href="mailto:pilipinastaekwondo@gmail.com"
+                            className="hover:underline text-sm text-gray-800"
+                        >
+                            pilipinastaekwondo@gmail.com
+                        </a>
+                    </div>
+
+                    <div className="flex items-center gap-2 mb-2">
+                        <FontAwesomeIcon icon={faPhone} className="w-4 h-4" />
+                        <span>+63 905 815 5032</span>
+                    </div>
                 </div>
+
 
             </div>
 
