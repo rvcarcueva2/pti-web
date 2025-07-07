ea9349c9c49bfb0d6808dde2c95a291edfeaf011
Reycel Carcueva <rvcarcueva2@student.apc.edu.ph>
2025-07-07 18:52:03 +0800

    modified page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,13 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen ">
 
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        
-      </div>
+
 
     </div>
+
+
   );
 
 }
