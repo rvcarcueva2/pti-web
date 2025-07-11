2a64648871a3c94fe540b2484207b81f03e3eb05
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2025-07-11 23:37:34 +0800

    modified home page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,13 +1,29 @@
 import HomeCarousel from './components/HomeCarousel';
 import FeaturedNewsSection from './components/FeaturedNewsSection';
+import Image from 'next/image';
+
 
 export default function Home() {
   return (
-    <main>
+    <main className="p-0 m-0">
+      {/* Hero Image */}
+      <div className="relative w-full overflow-hidden">
+        <div className="relative w-full h-[365px] sm:h-[465px] md:h-[585px] lg:h-[665px] xl:h-[745px]">
+          <Image
+            src="/images/Hero.png"
+            alt="Pilipinas Taekwondo Incorporated"
+            fill
+            priority
+            className="object-contain object-top"
+          />
+        </div>
+      </div>
+
       <HomeCarousel />
       <FeaturedNewsSection />
-      <div className="w-full h-[100px]" />
-      
+
+      {/* Bottom Spacer */}
+      <div className="w-full h-[60px]" />
     </main>
   );
 }
