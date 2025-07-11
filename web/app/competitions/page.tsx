3302fed11742f539d91e23c926860f0b3cce6fcd
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2025-07-11 21:11:16 +0800

    modified competitions page

--- web/app/competitions/page.tsx
+++ web/app/competitions/page.tsx
@@ -9,12 +9,20 @@ export default function Competitions() {
     return (
         <main className="px-4 sm:px-4 pt-16 pb-10 text-center">
             {/* Page Heading */}
-            <h1 className="font-poppins-black text-3xl border-b-4 border-[#FED018] inline-block pb-2">
-                COMPETITIONS
-            </h1>
+            <div className="text-center">
+                {/* h3 for mobile only */}
+                <h2 className="block md:hidden font-poppins-black text-2xl border-b-4 border-[#FED018] w-fit mx-auto pb-2">
+                    COMPETITIONS
+                </h2>
+
+                {/* h1 for desktop and up */}
+                <h1 className="hidden md:block font-poppins-black text-3xl border-b-4 border-[#FED018] w-fit mx-auto pb-2">
+                    COMPETITIONS
+                </h1>
+            </div>
 
             {/* List of Competition Posts */}
-            <div className="mt-16 space-y-6">
+            <div className="mt-8 md:mt-16 space-y-6">
                 {posts.map((post) => {
                     const postImage = getFirstImageFromPost(post.slug);
                     return (
@@ -31,27 +39,30 @@ export default function Competitions() {
                                     />
                                 </div>
                             )}
-                            <div className=" font-geist font-black text-xl mb-2">
+                            <div className="font-geist font-black text-base md:text-xl mb-0 md:mb-2">
                                 {post.meta.title}
                             </div>
-                            <div className="font-geist text-center">
-                                <p className="text-[#EAB044] font-semibold text-sm">{formatDate(post.meta.date)}</p>
 
+                            <div className="font-geist text-center">
+                                <span className="text-[#EAB044] font-semibold text-xs md:text-base">
+                                    {formatDate(post.meta.date)}
+                                </span>
                             </div>
 
-                            {/* Button positioned absolutely at bottom right */}
-                            <Link href={`/competitions/${post.slug}`}>
-                                <button className="absolute bottom-4 right-6 bg-foreground hover:bg-yellow-500 text-white font-semibold py-2 px-4 rounded-sm transition-colors duration-200 whitespace-nowrap flex items-center gap-2 cursor-pointer">
+                            <div className="mt-2 md:mt-0 md:absolute md:bottom-4 md:right-6 text-center md:text-right">
+                                <Link href={`/competitions/${post.slug}`}>
+                                    <button className="cursor-pointer bg-foreground hover:bg-yellow-500 text-white font-semibold py-2 px-4 rounded-sm transition-colors duration-200 whitespace-nowrap flex items-center gap-2 justify-center md:justify-end mx-auto md:mx-0">
                                     View Details
-                                    <Image
-                                        src="/icons/Forward Button.svg"
-                                        alt="Forward"
-                                        width={24}
-                                        height={24}
-                                        className="w-6 h-6"
-                                    />
-                                </button>
-                            </Link>
+                                        <Image
+                                            src="/icons/Forward Button.svg"
+                                            alt="Forward"
+                                            width={24}
+                                            height={24}
+                                            className="w-6 h-6"
+                                        />
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
                     );
                 })}
