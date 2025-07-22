81cdfecdc8f2b77724502b74e9c6015911a7e832
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2025-07-22 22:40:40 +0800

    modified dashboard page

--- web/app/admin-dashboard/dashboard/page.tsx
+++ web/app/admin-dashboard/dashboard/page.tsx
@@ -3,8 +3,7 @@
 export default function DashboardPage() {
   return (
     <div className="ml-64 p-6 font-geist">
-      <h1 className="text-2xl font-semibold text-gray-800 mb-2">Dashboard</h1>
-      <p className="text-gray-700">sample information</p>
+      <h1 className="text-2xl font-semibold text-gray-800 mb-2">Hello, Admin!</h1>
     </div>
   );
 }
