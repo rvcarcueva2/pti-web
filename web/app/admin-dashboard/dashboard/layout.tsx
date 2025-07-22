d92b4ac1ce978dda6e9bf7b5b2cf925cc2cb252d
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2025-07-22 20:43:54 +0800

    added dashboard page and layout

---
+++ web/app/admin-dashboard/dashboard/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "@/app/globals.css";
+import AdminDashboardSidebar from "@/app/components/AdminDashboardSidebar";
+
+export const metadata: Metadata = {
+    title: "Admin Dashboard | Pilipinas Taekwondo Inc.",
+  description:
+    "Admin Dashboard",
+};
+
+export default function SidebarLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen">
+      <AdminDashboardSidebar />
+      <main className="flex-1 bg-gray-50 p-8">
+        {children}
+      </main>
+    </div>
+  );
+}
