2cced9c3ab3c63e7b40db59ea7a779c31226b033
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2025-07-24 21:42:42 +0800

    modified lib

--- web/lib/fontawesome.ts
+++ web/lib/fontawesome.ts
@@ -1,9 +1,9 @@
 import { library, config } from "@fortawesome/fontawesome-svg-core";
-import { faEnvelope, faPhone, faCalendarDays, faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faPhone, faCalendarDays, faLocationDot, faClock } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faSquareInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 
 config.autoAddCss = false;
 
-library.add(faEnvelope, faPhone, faFacebook, faYoutube, faSquareInstagram, faCalendarDays, faLocationDot);
+library.add(faEnvelope, faPhone, faFacebook, faYoutube, faSquareInstagram, faCalendarDays, faLocationDot, faClock);
