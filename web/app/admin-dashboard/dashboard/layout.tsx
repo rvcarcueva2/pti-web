30d866866cbe5f83563f82d0382a71e17ac8797f
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2025-07-26 00:42:10 +0800

    modified layouts

--- web/app/admin-dashboard/dashboard/layout.tsx
+++ web/app/admin-dashboard/dashboard/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import "@/app/globals.css";
 import AdminDashboardSidebar from "@/app/components/AdminDashboardSidebar";
+import RequireAdmin from "@/app/components/RequireAdmin";
 
 export const metadata: Metadata = {
-    title: "Admin Dashboard | Pilipinas Taekwondo Inc.",
+  title: "Admin Dashboard | Pilipinas Taekwondo Inc.",
   description:
     "Admin Dashboard",
 };
@@ -14,11 +15,13 @@ export default function SidebarLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen">
-      <AdminDashboardSidebar />
-      <main className="flex-1 bg-gray-50 p-8">
-        {children}
-      </main>
-    </div>
+    <RequireAdmin>
+      <div className="flex min-h-screen">
+        <AdminDashboardSidebar />
+        <main className="flex-1 bg-gray-50 p-8">
+          {children}
+        </main>
+      </div>
+    </RequireAdmin>
   );
 }
