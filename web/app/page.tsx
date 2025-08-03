f131e548968f7d11f9b0d32003f1fa41872c9f88
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2025-08-03 22:23:14 +0800

    modified home page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -6,15 +6,26 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="p-0 m-0">
-      {/* Hero Image */}
       <div className="relative w-full overflow-hidden">
-        <div className="relative w-full h-[365px] sm:h-[465px] md:h-[585px] lg:h-[665px] xl:h-[745px]">
+        {/* Desktop Hero.svg */}
+        <div className="relative hidden sm:block w-full h-[365px] sm:h-[465px] md:h-[585px] lg:h-[665px] xl:h-[745px]">
           <Image
             src="/images/Hero.png"
             alt="Pilipinas Taekwondo Incorporated"
             fill
             priority
-            className="object-contain object-top"
+            className="object-contain object-top drop-shadow-lg"
+          />
+        </div>
+
+        {/* Mobile Hero2.svg */}
+        <div className="relative block sm:hidden w-full h-[365px]">
+          <Image
+            src="/images/Hero2.png"
+            alt="Pilipinas Taekwondo Incorporated"
+            fill
+            priority
+            className="object-contain object-top drop-shadow-lg"
           />
         </div>
       </div>
