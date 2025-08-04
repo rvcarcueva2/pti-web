9cbfdf2a92f66d1c51ad410a67a8c9a7c7732b5e
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2025-08-04 19:22:27 +0800

    modified root layout

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -46,6 +46,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppinsBlack.variable} ${poppinsRegular.variable} antialiased`}
+        suppressHydrationWarning={true}
       >
         <ResponsiveHeader /> {/* <-- Updated this */}
         {children}
