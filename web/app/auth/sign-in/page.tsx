75ebdf9e332e2b2a55f2c97bb91f970e8cc121b3
Reycel Carcueva <rvcarcueva2@student.apc.edu.ph>
2026-01-03 23:53:14 +0800

    modified auth pages

--- web/app/auth/sign-in/page.tsx
+++ web/app/auth/sign-in/page.tsx
@@ -199,37 +199,38 @@ const SignInForm: React.FC = () => {
           </div>
 
           {/* Password */}
-          <div className="relative">
+          <div>
             <label className="block text-sm font-semibold mb-1">
               Password <span className="text-[#D41716]">*</span>
             </label>
-            <input
-              type={showPassword ? 'text' : 'password'}
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-4 py-2 rounded bg-[#F9F8F8] border border-black/20 outline-none focus:ring-2 focus:ring-black"
-              style={{ paddingRight: '3rem' }} 
-            />
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"
-              tabIndex={-1}
-            >
-              <span className="relative block w-5 h-5">
-                <Image
-                  src={
-                    showPassword
-                      ? '/icons/hide-password.svg'
-                      : '/icons/show-password.svg'
-                  }
-                  alt={showPassword ? 'Hide password' : 'Show password'}
-                  fill
-                  className="object-contain"
-                />
-              </span>
-            </button>
+            <div className="relative">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full px-4 py-2 pr-12 rounded bg-[#F9F8F8] border border-black/20 outline-none focus:ring-2 focus:ring-black"
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"
+                tabIndex={-1}
+              >
+                <span className="relative block w-5 h-5">
+                  <Image
+                    src={
+                      showPassword
+                        ? '/icons/hide-password.svg'
+                        : '/icons/show-password.svg'
+                    }
+                    alt={showPassword ? 'Hide password' : 'Show password'}
+                    fill
+                    className="object-contain"
+                  />
+                </span>
+              </button>
+            </div>
             {errors.password && (
               <p className="text-sm text-[#D41716] mt-1">
                 {errors.password}
